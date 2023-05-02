1638d62c3d0e14b321270f1eec3c4d42c49b429e
Mary Laura Betancourt <mbetancourt@fi.uba.ar>
2023-05-02 16:34:56 -0400

    async function mock, no tested yet

--- src/services/LeagueService.js
+++ src/services/LeagueService.js
@@ -1,69 +1,86 @@
 /**
  * A class representing a service that processes the data for match schedule
  * and generates leaderboard.
- * 
- * NOTE: MAKE SURE TO IMPLEMENT ALL EXISITNG METHODS BELOW WITHOUT CHANGING THE INTERFACE OF THEM, 
- *       AND PLEASE DO NOT RENAME, MOVE OR DELETE THIS FILE.  
- * 
+ *
+ * NOTE: MAKE SURE TO IMPLEMENT ALL EXISITNG METHODS BELOW WITHOUT CHANGING THE INTERFACE OF THEM,
+ *       AND PLEASE DO NOT RENAME, MOVE OR DELETE THIS FILE.
+ *
  */
-class LeagueService {    
-    
-    /**
-     * Sets the match schedule.
-     * Match schedule will be given in the following form:
-     * [
-     *      {
-     *          matchDate: [TIMESTAMP],
-     *          stadium: [STRING],
-     *          homeTeam: [STRING],
-     *          awayTeam: [STRING],
-     *          matchPlayed: [BOOLEAN],
-     *          homeTeamScore: [INTEGER],
-     *          awayTeamScore: [INTEGER]
-     *      },
-     *      {
-     *          matchDate: [TIMESTAMP],
-     *          stadium: [STRING],
-     *          homeTeam: [STRING],
-     *          awayTeam: [STRING],
-     *          matchPlayed: [BOOLEAN],
-     *          homeTeamScore: [INTEGER],
-     *          awayTeamScore: [INTEGER]
-     *      }    
-     * ]
-     * 
-     * @param {Array} matches List of matches.
-     */    
-    setMatches(matches) {}
+import axios from "axios";
 
-    /**
-     * Returns the full list of matches.
-     * 
-     * @returns {Array} List of matches.
-     */
-    getMatches() {}
-
-    /**
-     * Returns the leaderboard in a form of a list of JSON objecs.
-     * 
-     * [     
-     *      {
-     *          teamName: [STRING]',
-     *          matchesPlayed: [INTEGER],
-     *          goalsFor: [INTEGER],
-     *          goalsAgainst: [INTEGER],
-     *          points: [INTEGER]     
-     *      },      
-     * ]       
-     * 
-     * @returns {Array} List of teams representing the leaderboard.
-     */
-    getLeaderboard() {}
+class LeagueService {
+  /**
+   * Sets the match schedule.
+   * Match schedule will be given in the following form:
+   * [
+   *      {
+   *          matchDate: [TIMESTAMP],
+   *          stadium: [STRING],
+   *          homeTeam: [STRING],
+   *          awayTeam: [STRING],
+   *          matchPlayed: [BOOLEAN],
+   *          homeTeamScore: [INTEGER],
+   *          awayTeamScore: [INTEGER]
+   *      },
+   *      {
+   *          matchDate: [TIMESTAMP],
+   *          stadium: [STRING],
+   *          homeTeam: [STRING],
+   *          awayTeam: [STRING],
+   *          matchPlayed: [BOOLEAN],
+   *          homeTeamScore: [INTEGER],
+   *          awayTeamScore: [INTEGER]
+   *      }
+   * ]
+   *
+   * @param {Array} matches List of matches.
+   */
+  setMatches(matches) {
     
-    /**
-     * Asynchronic function to fetch the data from the server.
-     */
-    async fetchData() {}    
+  }
+
+  /**
+   * Returns the full list of matches.
+   *
+   * @returns {Array} List of matches.
+   */
+  getMatches() {}
+
+  /**
+   * Returns the leaderboard in a form of a list of JSON objecs.
+   *
+   * [
+   *      {
+   *          teamName: [STRING]',
+   *          matchesPlayed: [INTEGER],
+   *          goalsFor: [INTEGER],
+   *          goalsAgainst: [INTEGER],
+   *          points: [INTEGER]
+   *      },
+   * ]
+   *
+   * @returns {Array} List of teams representing the leaderboard.
+   */
+  getLeaderboard() {}
+
+  /**
+   * Asynchronic function to fetch the data from the server.
+   */
+  async fetchData() {
+    const config = {
+      headers: {
+        Authorization: "Bearer YuHBdSlDXY000xa8IlCm7Qgq4_s",
+      },
+    };
+    const url = "http://localhost:3000/api/v1/getAllMatches";
+    try {
+      const apiResponse = await axios.get(url, config);
+      const matches = apiResponse.data;
+      setMatches(matches);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 export default LeagueService;
